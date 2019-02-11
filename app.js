a3d6b1de15f2f826cfbec8ff574e6c52fd6a7c7d
Hoa Vo <whoait@gmail.com>
2019-02-11 13:38:51 +0700

    test

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 // get all todos
 app.get('/api/v1/todos/:keyword', (req, res) => {
+  console.log('xxxxxxxxxxxxxxx');
   var keyword = req.params.keyword;
   console.log(keyword);
   googleTrends.interestOverTime({keyword: keyword})
